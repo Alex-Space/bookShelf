07588e1d38027f55815a8a7b32d1f313c42f48eb
Alex-Space <cometospace689@gmail.com>
2018-03-17 20:13:18 +0600

    Added api for books

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const app = express();
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+const {User} = require('./models/user');
+const {Book} = require('./models/book');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/api/getBook', (req, res) => {
+    let id = req.query.id;
+    Book.findById(id, (err, doc) => {
+        if (err) return res.status(400).send(err);
+        res.send(doc);
+    });
+});
+
+app.get('/api/books', (req, res) => {
+    // http://localhost:3001/api/books?skip=2&limit=10&order=asc
+    let skip = parseInt(req.query.skip);
+    let limit = parseInt(req.query.limit);
+    let order = req.query.order;
+
+    // ORDER = asc || desc
+    Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
+        if (err) return res.status(400).send(err);
+        res.send(doc);
+    });
+});
+
+app.post('/api/book_update', (req, res) => {
+    const value = {
+        name:
+    };
+    Book.findOneAndUpdate(req.body._id, req.body)
+});
+
+app.post('/api/book', (req, res) => {
+   const book = new Book(req.body);
+
+   book.save((err, doc) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).json({
+          post: true,
+          bookId: doc._id
+      });
+   });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+   console.log('SERVER RUNNING!');
+});
