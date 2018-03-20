07a89bd226b51b2f992c190dfec9378dbe67d025
Alex-Space <cometospace689@gmail.com>
2018-03-20 13:53:44 +0600

    Added review output for single book

--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -8,10 +8,28 @@ class BookView extends Component {
         this.props.dispatch(getBookWithRewiever(this.props.match.params.id));
     }
 
+    renderBooks = (books) => (
+        books.book ?
+            <div className="br_container">
+                <div className="br_header">
+                    <h2>{books.book.name}</h2>
+                    <h5>{books.book.author}</h5>
+                    <div className="br_reviewer">
+                        <span>Review by:</span> {books.reviewer.name} {books.reviewer.lastname}
+                    </div>
+                </div>
+                <div className="br_review">
+                    {books.book.review}
+                </div>
+            </div>
+            : null
+    );
+
     render() {
+        const books = this.props.books;
         return (
             <div>
-                book view
+                {this.renderBooks(books)}
             </div>
         );
     }
